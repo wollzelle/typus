8004bbe2f754437882df911b879722991d1d97bf
Thomas König <tkoenig@users.noreply.github.com>
2017-03-30 09:53:51 +0200

    Fix unpermitted params warnings

--- app/helpers/admin/resources/table_helper.rb
+++ app/helpers/admin/resources/table_helper.rb
@@ -62,7 +62,7 @@ module Admin::Resources::TableHelper
 
       {
         message: t(body),
-        url: params.permit.merge({controller: "/admin/#{model.to_resource}", id: item.id}).merge(url),
+        url: params.merge({controller: "/admin/#{model.to_resource}", id: item.id}).merge(url).permit!,
         options: options,
       }
     end.compact
